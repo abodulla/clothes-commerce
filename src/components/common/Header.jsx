6f53116993a91db19c79f91b50d119fc27d05c5a
abodulla <154546425+abodulla@users.noreply.github.com>
2025-01-26 22:18:41 +0300

    adding: -icons -Logo -links

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,16 +1,29 @@
+import logo from "../assets/logo.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faShoppingBasket } from "@fortawesome/free-solid-svg-icons";
+
 const Header = () => {
   return (
     <>
-      <div className="bg-gray-200 p-5">
-        <div className="flex items-space-between">
+      <div className=" h-20 flex justify-between">
+        <div className=" pl-4 flex  gap-4 gray-900  items-center object-start ">
           <a href="/">BUY T-SHIRTS</a>
           <a href="/">WOMEN</a>
           <a href="/">MEN</a>
           <a href="/">ABOUT</a>
           <a href="/">CONTACT</a>
         </div>
+        <div className=" flex object-center  object-cover">
+          <img src={logo} alt="Logo" className="w-45 h-20 " />
+        </div>
+        <div className="flex p-6 gap-10 object-end">
+          <FontAwesomeIcon icon={faMagnifyingGlass} className="gray-500" />
+          <p>$0.0</p>
+          <FontAwesomeIcon icon={faShoppingBasket} />
 
-        {/* <Logo className="flex items-center" /> */}
+          <a href="/">LOG IN</a>
+        </div>
       </div>
     </>
   );
