d03cbe738a7197d92c65cb6ca128591c84646248
abodulla <154546425+abodulla@users.noreply.github.com>
2025-01-26 19:05:51 +0300

    first commit

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,19 @@
+const Header = () => {
+  return (
+    <>
+      <div className="bg-gray-200 p-5">
+        <div className="flex items-space-between">
+          <a href="/">BUY T-SHIRTS</a>
+          <a href="/">WOMEN</a>
+          <a href="/">MEN</a>
+          <a href="/">ABOUT</a>
+          <a href="/">CONTACT</a>
+        </div>
+
+        {/* <Logo className="flex items-center" /> */}
+      </div>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/common/Header";
+
+const Home = () => {
+  return (
+    <>
+      <div className="">
+        <Header />
+      </div>
+    </>
+  );
+};
+
+export default Home;
