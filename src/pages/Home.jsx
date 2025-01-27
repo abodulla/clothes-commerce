221811d9ca7198dcf68aab17772eec9043947503
abodulla <154546425+abodulla@users.noreply.github.com>
2025-01-27 16:37:26 +0300

    adding Base Collection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Header from "../components/common/Header";
 import Hero from "../components/Home/Hero";
 import ProductGrid from "../components/Home/ProductGrid";
-
+import BaseCollection from "../components/Home/BaseCollection";
 const Home = () => {
   return (
     <>
@@ -9,6 +9,7 @@ const Home = () => {
         <Header />
         <Hero />
         <ProductGrid />
+        <BaseCollection />
       </div>
     </>
   );
